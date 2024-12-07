35727013c56bc35792f2566c538a8a115631692a
Anastasiia  Yushko <47480757+anastasiiayushko@users.noreply.github.com>
2024-12-07 19:42:02 +0200

    add vercel.json

--- src/settings.ts
+++ src/settings.ts
@@ -3,7 +3,7 @@ import {config} from "dotenv";
 config();
 
 export const SETTINGS_CONFIG = {
-    PORT: process.env.PORT || 3001,
+    PORT: process.env.PORT || 3000,
     PATH: {
         VIDEOS: "/videos",
     }
